876e5d6fac76c1e2ebbdb957a1642e7a0b30ddbf
xexuxjy <markneale@hotmail.com>
2023-12-16 11:55:02 +0000

    Didn't like that. got part 2 to work, but only after realising I had to ignore the original version which seems a bit wrong. also need to figure out why my match is broken again

--- 2023/Solutions2023/Solutions2023/Test13.cs
+++ 2023/Solutions2023/Solutions2023/Test13.cs
@@ -14,14 +14,14 @@ public class Test13 : BaseTest
     {
         TestID = 13;
         IsTestInput = false;
-        IsPart2 = false;
+        IsPart2 = true;
     }
 
-    void PrintGrid(List<string> grid)
+    void PrintGrid(List<char[]> grid)
     {
-        foreach (string line in grid)
+        foreach (char[] line in grid)
         {
-            DebugOutput(line);
+            DebugOutput(new string(line));
         }
 
         DebugOutput("");
@@ -29,76 +29,165 @@ public class Test13 : BaseTest
     
     public override void Execute()
     {
-        List<List<string>> patterns = new List<List<string>>();
-        List<string> patternData = new List<string>();
+        List<List<char[]>> patterns = new List<List<char[]>>();
+        List<char[]> patternData = new List<char[]>();
         patterns.Add(patternData);
 
         for (int i = 0; i < m_dataFileContents.Count; ++i)
         {
             if (m_dataFileContents[i] == "")
             {
-                patternData = new List<string>();
+                patternData = new List<char[]>();
                 patterns.Add(patternData);
             }
             else
             {
-                patternData.Add(m_dataFileContents[i]);
+                patternData.Add(m_dataFileContents[i].ToCharArray());
             }
         }
 
-        // List<string> test = new List<string>();
-        // test.Add("#....");
-        // test.Add("#....");
-        // test.Add(".....");
-        // test.Add("#....");
-        // test.Add("#.....");
-        // test.Add(".....");
+        // List<char[]> test = new List<char[]>();
+        // test.Add("#....".ToCharArray());
+        // test.Add("#....".ToCharArray());
+        // test.Add(".....".ToCharArray());
+        // test.Add("#....".ToCharArray());
+        // test.Add("#.....".ToCharArray());
+        // test.Add(".....".ToCharArray());
         //
         // PrintGrid(test);
-        // List<string> rotatedTest = RotatePattern(test);
+        // List<char[]> rotatedTest = RotatePattern(test);
         // PrintGrid(rotatedTest);
 
+        // int originalSccore = 
+        //
+        // for (int i = 0; i < 15; i++)
+        // {
+        //     List<char[]> a = CreateCopyWithChange(patterns[1],i);
+        //     List<char[]> r = RotatePattern(a);
+        //
+        //     int score = TryFindReflection(a, r, 0, 0, 0);
+        //     DebugOutput("Option " + i + " is " + score);
+        // }
+
+        if (IsPart2)
+        {
+            Part2(patterns);
+        }
+        else
+        {
+            Part1(patterns);
+        }
+    }
+
+    public void Part1(List<List<char[]>> patterns)
+    {
         int pos = 0;
         int totalScore = 0;
-        foreach (List<string> pattern in patterns)
+        int totalScoreCheck = 0;
+        
+        foreach (List<char[]> pattern in patterns)
         {
-            List<string> rotated = RotatePattern(pattern);
+            List<char[]> rotated = RotatePattern(pattern);
 
+            List<string> test = new List<string>();
+            foreach (char[] ca in pattern)
+            {
+                test.Add(new string(ca));
+            }
+
+            int testScore = CheckVerticalReflectionsString(test, pos);
+            
             int verticalScore = CheckVerticalReflections(pattern,pos);
             int horizontalScore = CheckVerticalReflections(rotated,pos) ;
 
-            int newScore = TryFindReflection(pattern, rotated, pos,horizontalScore,verticalScore);
-            totalScore += newScore;
+            int newScore = TryFindReflection(pattern, rotated, pos,0);
+            totalScoreCheck += newScore;
             
 
-            // if (verticalScore != 0)
-            // {
-            //     horizontalScore = 0;
-            // }
-            //
-            // if (verticalScore != 0 && horizontalScore != 0)
-            // {
-            //     DebugOutput("Got multiple lines of reflection for position " + pos+" V : "+verticalScore+"  H: "+horizontalScore);
-            //     foreach (string line in pattern)
-            //     {
-            //         DebugOutput(line);
-            //     }
-            //
-            //     DebugOutput("");
-            //     int ibreak = 0;
-            // }
+            if (verticalScore != 0)
+            {
+                horizontalScore = 0;
+            }
             
-            // totalScore += verticalScore;
-            // totalScore += (100 * horizontalScore);
+            totalScore += verticalScore;
+            totalScore += (100 * horizontalScore);
             
             
             pos++;
         }
 
-        DebugOutput("Total Score is : " + totalScore);
+        DebugOutput("Total Score for part 1 is : " + totalScore+"  check score is : "+totalScoreCheck);
+        
     }
 
-    public List<string> RotatePattern(List<string> original)
+    public List<char[]> CreateCopyWithChange(List<char[]> pattern, int option)
+    {
+        // create a copy.
+        List<char[]> patternCopy = new List<char[]>();
+        foreach (char[] cp in pattern)
+        {
+            patternCopy.Add(new string(cp).ToCharArray());
+        }
+
+        int y = option / pattern[0].Length;
+        int x = option % pattern[0].Length;
+
+        if (patternCopy[y][x] == '.')
+        {
+            patternCopy[y][x] = '#';
+        }
+        else
+        {
+            patternCopy[y][x] = '.';
+        }
+
+        return patternCopy;
+
+    }
+    
+    public void Part2(List<List<char[]>> patterns)
+    {
+        int pos = 0;
+        int totalScore = 0;
+        foreach (List<char[]> pattern in patterns)
+        {
+            int numOptions = pattern.Count * pattern[0].Length;
+            
+            int originalScore = TryFindReflection(pattern, RotatePattern(pattern), pos,0);             
+            
+            
+            for (int option = 0; option < numOptions; ++option)
+            {
+                // create a copy.
+                List<char[]> patternCopy = CreateCopyWithChange(pattern, option);
+                List<char[]> rotated = RotatePattern(patternCopy);
+
+                int verticalScore = CheckVerticalReflections(patternCopy, pos);
+                int horizontalScore = CheckVerticalReflections(rotated, pos);
+                //
+                // if (verticalScore != 0 || horizontalScore != 0)
+                // {
+                //     totalScore += verticalScore;
+                //     totalScore += (100 * horizontalScore);
+                //     break;
+                // }
+                int newScore = TryFindReflection(patternCopy, rotated, pos,originalScore);
+                totalScore += newScore;
+                
+                if (newScore != 0)
+                {
+                    // found a match
+                    break;
+                }
+            }
+            pos++;
+        }
+
+        DebugOutput("Total Score for part 2 is : " + totalScore);
+    }
+
+
+    public List<char[]> RotatePattern(List<char[]> original)
     {
         List<char[]> results = new List<char[]>();
 
@@ -112,21 +201,14 @@ public class Test13 : BaseTest
             {
                 results[i][j] = original[j][i];
             }
-            
         }
        
-        List<string> finalResults = new List<string>();
-        foreach (char[] ca in results)
-        {
-            finalResults.Add(new String(ca));
-        }
-
-        return finalResults;
+        return results;
     }
 
-    public int CheckVerticalReflections(List<string> originalPattern,int id)
+    public int CheckVerticalReflections(List<char[]> originalPattern,int id)
     {
-        int minPoints = 3;
+        int minPoints = 1;
         int endPoint = originalPattern[0].Length - minPoints;
         int midPoint = (originalPattern[0].Length) / 2;
         int points = midPoint;
@@ -140,6 +222,7 @@ public class Test13 : BaseTest
             int numPoints = 8;
             while (numPoints >= minPoints)
             {
+                matches = true;
                 for (int y = 0; y < originalPattern.Count; ++y)
                 {
                     for (int x = 0; x < numPoints; x++)
@@ -175,17 +258,72 @@ public class Test13 : BaseTest
 
         return foundPoint;
     }
+
+    
+    public int CheckVerticalReflectionsString(List<string> originalPattern,int id)
+    {
+        int minPoints = 3;
+        int endPoint = originalPattern[0].Length - minPoints;
+        int midPoint = (originalPattern[0].Length) / 2;
+        int points = midPoint;
+
+        int foundPoint = 0;
+
+        for (int reflectionPoint = 0; reflectionPoint < endPoint; ++reflectionPoint)
+        {
+            bool matches = true;
+            int numPoints = 8;
+            while (numPoints >= minPoints)
+            {
+                matches = true;
+                for (int y = 0; y < originalPattern.Count; ++y)
+                {
+                    for (int x = 0; x < numPoints; x++)
+                    {
+                        int pos1 = reflectionPoint- x;
+                        int pos2 = reflectionPoint + x + 1;
+
+                        if (pos1 < 0 || pos1 >= originalPattern[y].Length || pos2 < 0 ||
+                            pos2 >= originalPattern[y].Length || originalPattern[y][pos1] != originalPattern[y][pos2])
+                        {
+                            matches = false;
+                            break;
+                        }
+                        //DebugOutput($"Pos1 : {pos1+1}  Pos2 : {pos2+1}");
+                    }
+                }
+                if (matches)
+                {
+                    foundPoint = reflectionPoint+1;
+                    break;
+                }
+
+                numPoints--;
+            }
+
+            if (matches)
+            {
+                foundPoint = reflectionPoint+1;
+                break;
+            }
+        }
+
+        //DebugOutput("Final vertical result was : " + foundPoint);
+
+        return foundPoint;
+    }
+
     
     
     Dictionary<int, (int num, bool isRow)> initialMirrors= new(); 
-    private int TryFindReflection(List<string> asRows,List <string> asColumns, int Id,int hscore,int vscore)
+    private int TryFindReflection(List<char[]> asRows,List <char[]> asColumns, int Id,int originalScore)
     {
         //var asRows = block.SplitByNewline();
         //var asColumns = block.SplitIntoColumns().ToList();
         //Check rows
         for (int i = 1; i < asRows.Count; i++)
         {
-            if (asRows.Take(i).Reverse().Zip(asRows.Skip(i)).All(x => x.First == x.Second))
+            if (asRows.Take(i).Reverse().Zip(asRows.Skip(i)).All(x => x.First.SequenceEqual(x.Second)))
             {
                 if(initialMirrors.TryGetValue(Id, out (int num, bool isRow) x))
                 {
@@ -193,21 +331,22 @@ public class Test13 : BaseTest
                 }
                 initialMirrors[Id] = (i, true);
 
-                if (vscore != 0 && i != vscore)
-                {
-                    int ibreak = 0;
-                    DebugOutput($"Id {Id} Theirs {i}  Mine {vscore}  100 mult");
-                    PrintGrid(asRows);
-                }
+
                 
                 int result = i * 100;
+                // just want the new one
+                if (result == originalScore)
+                {
+                    continue;
+                }
+                
                 return result;
             }
         }
 
          for (int i = 1; i < asColumns.Count; i++)
          {
-             if (asColumns.Take(i).Reverse().Zip(asColumns.Skip(i)).All(x => x.First == x.Second))
+             if (asColumns.Take(i).Reverse().Zip(asColumns.Skip(i)).All(x => x.First.SequenceEqual(x.Second)))
              {
                  if (initialMirrors.TryGetValue(Id, out (int num, bool isRow) x))
                  {
@@ -215,11 +354,10 @@ public class Test13 : BaseTest
                          
                  }
                  initialMirrors[Id] = (i, false);
-                 if (hscore !=0 && i != hscore)
+                 // just want the new one
+                 if (i == originalScore)
                  {
-                     int ibreak = 0;
-                     DebugOutput($"Id {Id} Theirs {i}  Mine {hscore}");
-                     PrintGrid(asColumns);
+                     continue;
                  }
                  return i;
              }
