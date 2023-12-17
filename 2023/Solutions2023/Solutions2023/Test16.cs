35a751939b8bdb2d3b5c4c14881553581b0f43e8
xexuxjy <markneale@hotmail.com>
2023-12-17 21:09:36 +0000

    took a bit of messing, but pretty quick now

--- 2023/Solutions2023/Solutions2023/Test16.cs
+++ 2023/Solutions2023/Solutions2023/Test16.cs
@@ -6,7 +6,7 @@ public class Test16 : BaseTest
     public override void Initialise()
     {
         TestID = 16;
-        IsTestInput = true;
+        IsTestInput = false;
         IsPart2 = false;
     }
 
@@ -31,58 +31,83 @@ public class Test16 : BaseTest
         int escape = 100;
         int count = 0;
         int sameSetCount = 0;
+
+        int width = m_dataFileContents[0].Length;
+        int height = m_dataFileContents.Count;
+        
+        //HashSet<IntVector2> boardEnergisedSet = new HashSet<IntVector2>();
+        bool[] energised = new bool[width*height];
+    
+        int lastCount = energised.Count(x=>x==true);
+       
         while (true)
         {
-            HashSet<IntVector2> energisedSet = GetEnergisedSet(beams);
-
             foreach (Beam b in beams)
             {
                 if (!b.StillActive)
                 {
                     continue;
                 }
+
+                energised[(b.CurrentPosition.Y * width) + b.CurrentPosition.X] = true;
                 
+                char currentChar = GetContents(b.CurrentPosition);
+                if (currentChar == '/' || currentChar == '\\')
+                {
+                    b.CurrentDirection = Rotate(b.CurrentDirection, currentChar);
+                }
+                else if (currentChar == '|')
+                {
+                    if (b.CurrentDirection == IntVector2.Left || b.CurrentDirection == IntVector2.Right)
+                    {
+                        b.CurrentDirection = IntVector2.Up;
+                        // need to split.
+                        
+                        // add beams below but only if it makes sense
+                        if (InBounds(b.CurrentPosition + IntVector2.Down))
+                        {
+                            newBeams.Add(new Beam(b.CurrentPosition, IntVector2.Down));
+                        }
+                    }
+                }
+                else if (currentChar == '-')
+                {
+                    if (b.CurrentDirection == IntVector2.Up || b.CurrentDirection == IntVector2.Down)
+                    {
+                        b.CurrentDirection = IntVector2.Right;
+                        // need to split.
+                        // add beams left but only if it makes sense
+                        if (InBounds(b.CurrentPosition + IntVector2.Left))
+                        {
+                            newBeams.Add(new Beam(b.CurrentPosition, IntVector2.Left));
+                        }
+
+
+
+                    }
+                }
+
                 IntVector2 nextPos = b.CurrentPosition + b.CurrentDirection;
                 if (InBounds(nextPos))
                 {
                     b.Move();
-                    char nextChar = GetContents(nextPos);
-                    if (nextChar == '/' || nextChar == '\\')
-                    {
-                        b.CurrentDirection = Rotate(b.CurrentDirection, nextChar);
-                    }
-                    else if (nextChar == '|')
-                    {
-                        if (b.CurrentDirection == IntVector2.Left || b.CurrentDirection == IntVector2.Right)
-                        {
-                            b.CurrentDirection = IntVector2.Up;
-                            // need to split.
-                            newBeams.Add(new Beam(nextPos,IntVector2.Down));
-                        }
-                    }
-                    else if (nextChar == '-')
-                    {
-                        if (b.CurrentDirection == IntVector2.Up || b.CurrentDirection == IntVector2.Down)
-                        {
-                            b.CurrentDirection = IntVector2.Right;
-                            // need to split.
-                            newBeams.Add(new Beam(nextPos,IntVector2.Left));
-                        }
-                    }
                 }
                 else
                 {
                     b.Stop();
                 }
+
+                energised[(b.CurrentPosition.Y * width) + b.CurrentPosition.X] = true;
+
             }
 
             beams.AddRange(newBeams);
             newBeams.Clear();
 
 
-            HashSet<IntVector2> newEnergisedSet = GetEnergisedSet(beams);
-
-            if (newEnergisedSet.SequenceEqual(energisedSet))
+            int curentCount = energised.Count(x=>x==true);
+            
+            if(curentCount == lastCount)
             {
                 sameSetCount++;
                 if (sameSetCount > 3)
@@ -92,64 +117,31 @@ public class Test16 : BaseTest
             }
             else
             {
+                lastCount = curentCount;
                 sameSetCount = 0;
             }
-
-
-            // DebugOutput(DebugGrid(beams));
-            //
-            // bool beamsActive = false;            
-            //
-            // foreach (Beam b in beams)
-            // {
-            //     if (b.StillActive)
-            //     {
-            //         beamsActive = true;
-            //         break;
-            //     }
-            // }
-            //
-            // if (!beamsActive)
-            // {
-            //     break;
-            // }
-            //
-            // count++;
-            // if (count > escape)
-            // {
-            //     break;
-            // }
-
         }
 
-        HashSet<IntVector2> finalResults = GetEnergisedSet(beams);
-        
-        
-        
-        
-        DebugOutput("Number of cells energized is : " + finalResults.Count);
-        DebugOutput(DebugGrid(finalResults));
-    }
-
-    public HashSet<IntVector2> GetEnergisedSet(List<Beam> beams)
-    {
-        HashSet<IntVector2> finalResults = new HashSet<IntVector2>();
-        foreach (Beam b in beams)
-        {
-            foreach (IntVector2 pos in b.PreviousPositions)
-            {
-                finalResults.Add(pos);
-            }
-        }
-
-        return finalResults;
-    }
-
-    public String DebugGrid(List<Beam> beams)
-    {
-        return DebugGrid(GetEnergisedSet(beams));
-    }
     
+
+        DebugOutput("Number of cells energized is : " + lastCount);
+        DebugOutput(DebugGrid(energised,width,height));
+    }
+
+    // public HashSet<IntVector2> GetEnergisedSet(List<Beam> beams)
+    // {
+    //     HashSet<IntVector2> finalResults = new HashSet<IntVector2>();
+    //     foreach (Beam b in beams)
+    //     {
+    //         foreach (IntVector2 pos in b.PreviousPositions)
+    //         {
+    //             finalResults.Add(pos);
+    //         }
+    //     }
+    //
+    //     return finalResults;
+    // }
+
     public string DebugGrid(HashSet<IntVector2> positions)
     {
         StringBuilder sb = new StringBuilder();
@@ -165,6 +157,22 @@ public class Test16 : BaseTest
 
         return sb.ToString();
     }
+
+    public string DebugGrid(bool[] grid,int width,int height)
+    {
+        StringBuilder sb = new StringBuilder();
+        for (int y = 0; y < height; ++y)
+        {
+            for (int x = 0; x < width; ++x)
+            {
+                sb.Append(grid[(y*width)+x] ? '#' : '.');
+            }
+            sb.AppendLine();
+        }
+
+        return sb.ToString();
+    }
+
     
     public IntVector2 Rotate(IntVector2 direction, char symbol)
     {
@@ -189,7 +197,7 @@ public class Test16 : BaseTest
         //     return new IntVector2(-direction.Y, direction.X);
         // }
     }
-    
+
     public bool InBounds(IntVector2 v)
     {
         return v.X >= 0 && v.Y >= 0 && v.X < m_dataFileContents[0].Length && v.Y < m_dataFileContents.Count;
@@ -199,18 +207,18 @@ public class Test16 : BaseTest
     {
         return m_dataFileContents[v.Y][v.X];
     }
-    
+
 
     public class Beam
     {
         public static int count = 0;
         public IntVector2 CurrentDirection = new IntVector2();
         public IntVector2 CurrentPosition = new IntVector2();
-        public List<IntVector2> PreviousPositions = new List<IntVector2>();
-
+ 
         private bool m_stopped = false;
-        
+
         private int id = count++;
+
         public Beam(IntVector2 initialPosition, IntVector2 initialDirection)
         {
             CurrentDirection = initialDirection;
@@ -219,21 +227,17 @@ public class Test16 : BaseTest
 
         public void Move()
         {
-            PreviousPositions.Add(CurrentPosition);
             CurrentPosition += CurrentDirection;
         }
 
         public void Stop()
         {
-            PreviousPositions.Add(CurrentPosition);
             m_stopped = true;
         }
-        
+
         public bool StillActive
         {
             get { return !m_stopped; }
         }
-        
     }
-    
 }
