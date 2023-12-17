6aa018176c2e924e584f5981ebdad73a35a25a7c
xexuxjy <markneale@hotmail.com>
2023-12-17 10:19:01 +0000

    Part1

---
+++ 2023/Solutions2023/Solutions2023/Test15.cs
@@ -0,0 +1,45 @@
+﻿using System.Text;
+
+public class Test15 : BaseTest
+{
+    public override void Initialise()
+    {
+        TestID = 15;
+        IsTestInput = false;
+        IsPart2 = false;
+    }
+
+    public int GenerateHASH(string val)
+    {
+        int total = 0;
+        foreach (char c in val)
+        {
+            int asciiVal = (int)c;
+            total += c;
+            total *= 17;
+            total = total % 256;
+        }
+
+        return total;
+    }
+
+    public override void Execute()
+    {
+        long finalTotal = 0;
+        List<string> parseResuls = new List<string>();
+        foreach (string line in m_dataFileContents)
+        {
+            string[] tokens = line.Split(',', StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
+            foreach (string token in tokens)
+            {
+                finalTotal += GenerateHASH(token);
+            }
+        }
+
+        DebugOutput("Final total is " + finalTotal);
+    }
+    
+    
+    
+    
+}
