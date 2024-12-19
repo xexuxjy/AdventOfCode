bd4faa9df072fb787c2b665a4c2386c8c5e0e495
xexuxjy <markneale@hotmail.com>
2024-12-19 22:34:01 +0000

    temp - working but very slow

--- 2024/Test19_2024.cs
+++ 2024/Test19_2024.cs
@@ -1,5 +1,9 @@
-﻿using System.Numerics;
+﻿using System;
+using System.Collections.Concurrent;
+using System.Numerics;
+using System.Reflection.Metadata;
 using System.Xml.Linq;
+using static Test13_2024;
 
 public class Test19_2024 : BaseTest
 {
@@ -34,12 +38,12 @@ public class Test19_2024 : BaseTest
 
         List<string> reducedList = new List<string>();
         // remove the patterns that can be made from other patterns?
-        foreach(string availablePattern in sortedPatterns)
+        foreach (string availablePattern in sortedPatterns)
         {
             List<string> patternCopy = new List<string>();
             patternCopy.AddRange(sortedPatterns);
             patternCopy.Remove(availablePattern);
-            if(!TestPossibility(availablePattern,patternCopy))
+            if (!TestPossibility(availablePattern, patternCopy))
             {
                 reducedList.Add(availablePattern);
             }
@@ -48,6 +52,8 @@ public class Test19_2024 : BaseTest
 
 
 
+        List<string> possiblePatterns = new List<string>();
+
         int count = 0;
         int score = 0;
         foreach (string targetPattern in targetPatterns)
@@ -55,6 +61,7 @@ public class Test19_2024 : BaseTest
             bool foundPossible = TestPossibility(targetPattern, reducedList);
             if (foundPossible)
             {
+                possiblePatterns.Add(targetPattern);
                 score++;
             }
             DebugOutput($"Testing pattern {count}  valid {foundPossible} ");
@@ -65,7 +72,98 @@ public class Test19_2024 : BaseTest
         DebugOutput($"{score} out of {targetPatterns.Count}  are possible.");
         int ibreak = 0;
 
+
+        if (IsPart2)
+        {
+
+            ConcurrentBag<long> bagResults = new ConcurrentBag<long>();
+            long totalScore = 0;
+            Parallel.ForEach(possiblePatterns, new ParallelOptions { MaxDegreeOfParallelism = 6 }, pattern =>
+            {
+                DateTime startTime = DateTime.Now;
+                DebugOutput($"Testing {pattern}");
+
+                long localScore = TestPossibility2(pattern, sortedPatterns);
+                bagResults.Add(localScore);
+                DateTime endTime = DateTime.Now;
+                double bpElapsed = DateTime.Now.Subtract(startTime).TotalMilliseconds;
+                DebugOutput("Elapsed = " + bpElapsed + " ms");
+
+                DebugOutput($"Pattern {pattern} can be made in {localScore} ways");
+            });
+
+            foreach (long b in bagResults)
+            {
+                totalScore += b;
+            }
+
+            //var tasks = new List<Task<long>>(possiblePatterns.Count);
+            //foreach (string pattern in possiblePatterns)
+            //{
+            //    tasks.Add(Task<long>.Factory.StartNew(() =>
+            //    {
+            //        DateTime startTime = DateTime.Now;
+            //        DebugOutput($"Testing {pattern}");
+            //        long localScore = TestPossibility2(pattern, sortedPatterns);
+            //        DebugOutput($"Pattern {pattern} can be made in {localScore} ways");
+            //        DateTime endTime = DateTime.Now;
+            //        double bpElapsed = DateTime.Now.Subtract(startTime).TotalMilliseconds;
+            //        DebugOutput("Elapsed = " + bpElapsed + " ms");
+            //        return localScore;
+            //    }));
+            //}
+
+
+
+            //foreach (Task<long> task in tasks)
+            //{
+            //    totalScore += task.Result;
+            //}
+
+
+
+            DebugOutput("Total score is " + totalScore);
+
+        }
+
+
     }
+    public long TestPossibility2(string targetPattern, List<string> availablePatterns)
+    {
+        long count = 0;
+        if (targetPattern == "")
+        {
+            return 1;
+        }
+
+        List<string> reducedList = new List<string>();
+
+        foreach (string s in availablePatterns)
+        {
+            if (targetPattern.Contains(s))
+            {
+                reducedList.Add(s);
+            }
+        }
+
+        bool hasMatch = false;
+        foreach (string s in reducedList)
+        {
+            if (s.Length > targetPattern.Length)
+            {
+                return count;
+            }
+
+            //DebugOutput($"Testing {targetPattern} with {s} matches {targetPattern.StartsWith(s)}");
+            if (targetPattern.StartsWith(s))
+            {
+                count += TestPossibility2(targetPattern.Substring(s.Length), reducedList); ;
+            }
+        }
+        return count;
+    }
+
+
 
     // to start with, don;t need to know what the pattern is, just that one exists
     public bool TestPossibility(string targetPattern, List<string> availablePatterns)
@@ -78,7 +176,7 @@ public class Test19_2024 : BaseTest
         bool hasMatch = false;
         foreach (string s in availablePatterns)
         {
-            if(s.Length > targetPattern.Length)
+            if (s.Length > targetPattern.Length)
             {
                 return false;
             }
@@ -86,8 +184,8 @@ public class Test19_2024 : BaseTest
             //DebugOutput($"Testing {targetPattern} with {s} matches {targetPattern.StartsWith(s)}");
             if (targetPattern.StartsWith(s))
             {
-                bool match =TestPossibility(targetPattern.Substring(s.Length), availablePatterns);; 
-                if(match)
+                bool match = TestPossibility(targetPattern.Substring(s.Length), availablePatterns); ;
+                if (match)
                 {
                     return true;
                 }
@@ -100,5 +198,60 @@ public class Test19_2024 : BaseTest
     // need to reduce the set of possible types.
 
 
+    public class TrieNode
+    {
+        public bool endOfWord = false;
+        public TrieNode[] children = new TrieNode[26];
+    }
 
+    public class Trie
+    {
+        private TrieNode root = new TrieNode();
+
+        // Insert a string into the trie
+        public void Insert(string s)
+        {
+            TrieNode prev = root;
+            foreach (char c in s)
+            {
+                int index = c - 'a';
+                if (prev.children[index] == null)
+                    prev.children[index] = new TrieNode();
+                prev = prev.children[index];
+            }
+            prev.endOfWord = true;
+        }
+
+        // Find the number of ways to form the given string
+        // using the strings in the trie
+        public int WaysOfFormingString(string str)
+        {
+            int n = str.Length;
+            int[] count = new int[n];
+
+            // For each index i in the input string
+            for (int i = 0; i < n; i++)
+            {
+                TrieNode ptr = root;
+                // Check all possible substrings of str ending
+                // at index i
+                for (int j = i; j >= 0; j--)
+                {
+                    char ch = str[j];
+                    int index = ch - 'a';
+                    if (ptr.children[index] == null)
+                        break;
+                    ptr = ptr.children[index];
+                    if (ptr.endOfWord)
+                        // If the substring ending at index j is
+                        // in the trie, update the count
+                        count[i] += j > 0 ? count[j - 1] : 1;
+                }
+            }
+
+            // The final count is the number of ways to form the
+            // entire string
+            return count[n - 1];
+        }
+    }
 }
--- Program.cs
+++ Program.cs
@@ -6,9 +6,9 @@ public class Program
     static void Main()
     {
         //new Test19_2024().SetTestInput().RunTest();
-        new Test19_2024().RunTest(); 
+        //new Test19_2024().RunTest(); 
         //new Test19_2024().SetTestInput().SetPart2().RunTest(); 
-        //new Test19_2024().SetPart2().RunTest(); 
+        new Test19_2024().SetPart2().RunTest(); 
     }
 
 
