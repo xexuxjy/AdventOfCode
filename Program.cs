e0158a7d6f088223b1d3bb16623cddc50b15d984
xexuxjy <markneale@hotmail.com>
2025-11-27 23:18:49 +0000

    And day 25!

--- Program.cs
+++ Program.cs
@@ -6,10 +6,10 @@ public class Program
 
     static void Main()
     {
-        //new Test11_2016().SetTestInput().RunTest(); 
-        //new Test11_2016().RunTest(); 
-        //new Test11_2016().SetTestInput().SetPart2().RunTest(); 
-        new Test11_2016().SetPart2().RunTest(); 
+        new Test25_2016().SetTestInput().RunTest(); 
+        //new Test25_2016().RunTest(); 
+        //new Test25_2016().SetTestInput().SetPart2().RunTest(); 
+        //new Test25_2016().SetPart2().RunTest(); 
     }
 
     
